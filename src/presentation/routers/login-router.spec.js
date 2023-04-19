7e4f39eb2499c0310737698aa0eb9fddfb291349
João Miguel <joaomiguelmv_04@hotmail.com>
2023-04-19 11:35:24 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -84,7 +84,7 @@ describe('Login Router', () => {
     })
 
     test('Should return 200 valid credentials are provided', () => {
-        const { sut } = makeSut()
+        const { sut, authUseCaseSpy } = makeSut()
         const httpRequest = {
             body: {
                 email: 'valid_email@hotmail.com',
@@ -93,6 +93,8 @@ describe('Login Router', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
+
     })
 
     test('Should return 500 if no AuthUseCase is provided', () => {
