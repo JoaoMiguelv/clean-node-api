2dffbede3c500d3f19fb2d50405098e6eac3b613
João Miguel <joaomiguelmv_04@hotmail.com>
2023-04-19 14:14:50 -0300

    add jest config

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -30,7 +30,7 @@ module.exports = class LoginRouter {
             }
             return HttpResponse.ok({ accessToken })
         } catch (error) {
-            //console.error(error)
+            console.error(error)
             return HttpResponse.serverError()
         }
 
