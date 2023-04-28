f0a186c8c1045fd41fd3eb12cfd271b71ee317d7
João Miguel <joaomiguelmv_04@hotmail.com>
2023-04-28 16:17:06 -0300

    move error to a custom folder

---
+++ src/presentation/errors/index.js
@@ -0,0 +1,11 @@
+const MissingParamError = require('../errors/missing-param-error')
+const UnauthorizedError = require('../errors/unauthorized-error')
+const ServerError = require('../errors/server-error')
+const InvalidParamError = require('../errors/invalid-param-error')
+
+module.exports = {
+  MissingParamError,
+  UnauthorizedError,
+  ServerError,
+  InvalidParamError
+}
