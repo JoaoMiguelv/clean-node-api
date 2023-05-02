634a0be3258a7f7f77b0fe167ae0b73f0f6687e4
João Miguel <joaomiguelmv_04@hotmail.com>
2023-05-02 11:00:48 -0300

    ensure AuthUseCase throws if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,15 @@
+class AuthUseCase {
+  async auth(email, password) {
+    if (!email) {
+      throw new Error()
+    }
+  }
+}
+
+describe('Auth UseCase', () => {
+  test('Should throws if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})
